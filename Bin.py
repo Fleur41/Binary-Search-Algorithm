4e8e88c74b8b31fde5b77c15bf49777eea8037a0
Fleur41 <Muriithikings456@gmail.com>
2023-12-30 11:36:01 +0300

    binary search rec

--- Bin.py
+++ Bin.py
@@ -6,3 +6,23 @@ Binary Search
 def binary_search(a_list, an_item):
     first = 0
     last = len(a_list) - 1
+
+    while first <= last:
+        mid_point = (first + last) // 2
+        if a_list[mid_point] == an_item:
+            return True
+        else:
+            if an_item < a_list[mid_point]:
+                last = mid_point - 1
+            else:
+                first = mid_point + 1
+
+    return False
+
+def binary_search_rec(a_list, first, last, an_item):
+    if len(a_list) == 0:
+        return False
+    else:
+        mid_point = (first + last) // 2
+        if a_list[mid_point] == an_item:
+            return True

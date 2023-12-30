4dac3e7b9eea1d2d38cef7d6eea920d36085e13e
Fleur41 <Muriithikings456@gmail.com>
2023-12-30 11:44:30 +0300

    Final binary search rec

--- Bin.py
+++ Bin.py
@@ -26,3 +26,17 @@ def binary_search_rec(a_list, first, last, an_item):
         mid_point = (first + last) // 2
         if a_list[mid_point] == an_item:
             return True
+        else:
+            if an_item < a_list[mid_point]:
+                last = mid_point - 1
+                return binary_search_rec(a_list, first, last, an_item)
+            else:
+                first = mid_point + 1
+                return binary_search_rec(a_list, first, last, an_item)
+            
+if __name__ == '__main__':
+    a_list = [1, 4, 7, 10, 14, 19, 102, 2575, 10000]
+
+    print('Binary Search:', binary_search(a_list, 4))
+    print('Binary Search Recursive:',
+       binary_search_rec(a_list, 0, len(a_list) -1, 4))
